cd7a91b9c2b3ac9ebf180858b42a8dffb7f5a44a
Barterr <bartgb00@gmail.com>
2017-03-31 13:51:59 -0300

    Terminada primeira versao do algoritmo, acha a solucao para alguns estados

--- src/IA.js
+++ src/IA.js
@@ -1,11 +1,24 @@
-import { Algoritmo } from 'Algoritmo.js';
+import { Algoritmo } from './Algoritmo.js';
 // import { Caminho } from 'Caminho.js';
 
+// ESTADO DIFICIL PRA CARALHO
 let estadoInicial = [
   [1, 3, 4],
-  [6, 5, 7],
-  [2, 8, 0]
+  [6, 7, 5],
+  [8, 2, 0]
 ];
+
+// let estadoInicial = [
+//   [1, 3, 4],
+//   [6, 7, 5],
+//   [2, 8, 0]
+// ];
+
+// let estadoInicial = [
+//   [6, 0, 8],
+//   [3, 1, 4],
+//   [2, 7, 5]
+// ];
 
 let solucao = [
   [1, 2, 3],
@@ -16,4 +29,5 @@ let solucao = [
 // let caminho = new Caminho('', estadoInicial, solucao);
 // console.log(caminho);
 
-Algoritmo(solucao, estadoInicial);
+let caminho = Algoritmo(solucao, estadoInicial);
+console.log(caminho);
