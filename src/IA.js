f4e85ce5b45384fc9dd94fbd158669ded1dfb2d8
Barterr <bartgb00@gmail.com>
2017-04-04 17:28:45 -0300

    Adicionada funcao p/ achar varias solucoes, retornando a melhor

--- src/IA.js
+++ src/IA.js
@@ -1,33 +1,24 @@
-import { Algoritmo } from './Algoritmo.js';
+const alg = require('./Algoritmo.js');
 // import { Caminho } from 'Caminho.js';
-
-// ESTADO DIFICIL PRA CARALHO
-let estadoInicial = [
-  [1, 3, 4],
-  [6, 7, 5],
-  [8, 2, 0]
-];
 
 // let estadoInicial = [
 //   [1, 3, 4],
 //   [6, 7, 5],
-//   [2, 8, 0]
+//   [8, 2, 0]
 // ];
 
-// let estadoInicial = [
-//   [6, 0, 8],
-//   [3, 1, 4],
-//   [2, 7, 5]
-// ];
+let estadoInicial = [
+  [2, 3, 8],
+  [4, 1, 6],
+  [5, 7, 0]
+];
 
 let solucao = [
   [1, 2, 3],
-  [8, 0, 4],
-  [7, 6, 5]
+  [4, 5, 6],
+  [7, 8, 0]
 ];
 
-// let caminho = new Caminho('', estadoInicial, solucao);
-// console.log(caminho);
 
-let caminho = Algoritmo(solucao, estadoInicial);
+let caminho = alg.Resolver8Puzzle(estadoInicial);
 console.log(caminho);
